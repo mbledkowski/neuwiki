23c7b2d29153b0da8efee03c4e0ebc6203b311de
Maciej Błędkowski <pub@mble.dk>
2022-07-13 23:22:35 +0200

    Add database setup script

--- back/src/orbit/orbit.module.ts
+++ back/src/orbit/orbit.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { OrbitService } from './orbit.service';
-import { OrbitController } from './orbit.controller';
 
 @Module({
   providers: [OrbitService],
-  controllers: [OrbitController],
 })
 export class OrbitModule {}
