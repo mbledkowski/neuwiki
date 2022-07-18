be53a316fdf30e48ea397de8431a752023347b1b
Maciej Błędkowski <pub@mble.dk>
2022-07-18 10:27:27 +0200

    Change name of a variable

--- back/src/auth/strategy/jwt.strategy.ts
+++ back/src/auth/strategy/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: { sub: string; email: string }) {
+  async validate(payload: { sub: string; wallet: string }) {
     const user = await this.prisma.user.findUnique({
       where: {
         id: payload.sub,
