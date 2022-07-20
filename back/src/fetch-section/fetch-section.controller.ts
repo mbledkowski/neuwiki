69e048e13a9dc5d4580ad2225f56cc17dc1b35e1
Maciej Błędkowski <pub@mble.dk>
2022-07-20 15:34:34 +0200

    Fix required field in swagger for fetch section

--- back/src/fetch-section/fetch-section.controller.ts
+++ back/src/fetch-section/fetch-section.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
 import { FetchSectionService } from './fetch-section.service';
 
 @Controller('fetch')
@@ -6,6 +7,8 @@ export class FetchSectionController {
   constructor(private readonly fetchSectionService: FetchSectionService) { }
 
   @Get('')
+  @ApiQuery({ name: 'id', type: String, required: false })
+  @ApiQuery({ name: 'urlName', type: String, required: false })
   async fetchSection(@Query('id') id: string, @Query('urlName') urlName: string) {
     return this.fetchSectionService.fetchSection({ id, urlName });
   }
